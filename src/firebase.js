ff738e056dbaadf18cf692549bcb2be24d841d5c
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-24 23:06:14 +0530

    Remove sensitive Firebase configuration
    Removed Firebase configuration details from firebase.js.

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwVqinePgkAcCiGOoYt5SEOgHyVyprXyc",
-  authDomain: "smart-agri-af9b0.firebaseapp.com",
-  projectId: "smart-agri-af9b0",
-  storageBucket: "smart-agri-af9b0.firebasestorage.app",
-  messagingSenderId: "1072695872191",
-  appId: "1:1072695872191:web:da38edf9cf166dea83140a"
+
 };
 
 const app = initializeApp(firebaseConfig);
